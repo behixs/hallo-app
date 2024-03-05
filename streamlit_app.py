919079ee1c7e973b597d76f1a842ccacccae0886
behixs <161949690+behixs@users.noreply.github.com>
2024-03-05 11:54:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 st.write("Hallo Welt")
 
+st.write("""
+# Simple Iris Flower Prediction App 
+
+This App predicts the Iris flower type!
+""")
+st.sidebar.header('User Input Parameters')
+
+         
+
