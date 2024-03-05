a4630df958340ba5dad6bfb29e945f86878e4a66
behixs <161949690+behixs@users.noreply.github.com>
2024-03-05 17:36:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
-st.write("Hallo Welt")
 
+def main():
+    st.title("Begrüßungs-App")
+    
+    # Eingabefeld für den Namen des Benutzers
+    name = st.text_input("Gib deinen Namen ein:")
+    
+    # Überprüfen, ob der Benutzer einen Namen eingegeben hat
+    if name:
+        st.write(f"Hallo, {name}! Willkommen zur interaktiven Streamlit-App.")
+    else:
+        st.write("Bitte gib deinen Namen ein.")
+
+if __name__ == "__main__":
+    main()
 
 
 
